0d8ce68e2c108a1e36ce2aced381290486260016
Vxrcel <skyeparagon@gmail.com>
2023-11-16 10:13:43 +0100

    Blog Preview

--- src/components/admin/CreateBlog.tsx
+++ src/components/admin/CreateBlog.tsx
@@ -114,7 +114,7 @@ const CreateBlog = () => {
     event.preventDefault();
     setIsLoading(true);
     const formId = toast.loading("Publishing post...");
-
+    localStorage?.setItem("formData", JSON.stringify(formData));
     try {
       const res = await fetch("/api/blog-post", {
         method: "POST",
@@ -363,7 +363,10 @@ const CreateBlog = () => {
           <div className="hidden md:flex my-10 lg:my-20 w-full justify-center gap-x-8 [&>*]:text-2xl [&>*]:p-4 [&>*]:px-12 [&>*]:rounded-xl font-Inter">
             <button
               type="button"
-              onClick={() => setOpenPreview(true)}
+              onClick={() => {
+                localStorage?.setItem("formData", JSON.stringify(formData));
+                setOpenPreview(true);
+              }}
               role="dialog"
               aria-modal="true"
               disabled={formData.content.length === 0}
