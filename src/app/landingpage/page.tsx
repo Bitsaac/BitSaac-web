0b64f25e623cc31fdb244950d5d25b4f01037d43
cybertronbot <okhuomonajayi54@gmail.com>
2023-11-17 19:56:01 +0100

    avi-fix

---
+++ src/app/landingpage/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+
+
+const page = () => {
+  return (
+    <div className='bg-white w-full'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default page
